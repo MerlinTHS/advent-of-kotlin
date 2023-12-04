3bcb0fa2448a00b90d057cf3f51ebedd6b0dd618
Merlin Eden <m.eden@ths-bs.de>
2023-12-05 00:33:24 +0100

    Day 4

---
+++ src/Day04.kt
@@ -0,0 +1,43 @@
+import kotlin.math.pow
+
+val scratchcards = readLines("Day04")
+
+fun main() {
+    partOne()
+    partTwo()
+}
+
+fun partOne() {
+    val totalPoints = scratchcards.sumOf { it.correctNumbers.points }
+    println("Total points: $totalPoints")
+}
+
+fun partTwo() {
+    val correctNumbers = scratchcards.map { it.correctNumbers }
+    val copies = MutableList(correctNumbers.size) { 1 }
+
+    for (card in copies.indices) {
+        repeat (copies[card]) {
+            repeat (correctNumbers[card]) {
+                if (card+it+1 <= copies.lastIndex) copies[card+it+1]++
+            }
+        }
+    }
+
+    println("Total cards: ${copies.sum()}")
+}
+
+val String.correctNumbers: Int get() =
+    givenNumbers.intersect(winningNumbers).size
+
+val Int.points: Int get() =
+    (2.0.pow(this) / 2).toInt()
+
+val String.givenNumbers get() =
+    Regex("(?<=\\|).*\$").find(this)?.numbers.orEmpty()
+
+val String.winningNumbers get() =
+    Regex("(?<=:)(.*)(?=\\|)").find(this)?.numbers.orEmpty()
+
+val MatchResult.numbers: Set<Int> get() =
+    Regex("\\d+").findAll(value).map { it.value.toInt() }.toSet()
